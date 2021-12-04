07266e21e1be2a403f62a9b210dd00abeef2f44b
BlackGlory <woshenmedoubuzhidao@blackglory.me>
2021-12-04 11:07:45 +0000

    chore: remove rules

--- .eslintrc.js
+++ .eslintrc.js
@@ -13,7 +13,6 @@ module.exports = {
   , 'no-async-promise-executor': 'off'
   , '@typescript-eslint/no-inferrable-types': 'off'
   , '@typescript-eslint/ban-types': 'off'
-  , '@typescript-eslint/no-empty-function': 'off'
   , '@typescript-eslint/ban-ts-comment': 'off'
   }
 }
